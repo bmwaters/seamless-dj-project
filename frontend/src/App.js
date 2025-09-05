79c42c2cde02169975b58a9fac450c6ecc13bb42
bmwaters <bmwaters@arizona.edu>
2025-09-05 14:29:02 -0400

    Add react frontend skeleton for Seamless DJ Project

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>Seamless DJ Project Frontend</h1>
+      <p>Controls and playlists UI will go here.</p>
+    </div>
+  );
+}
+
+export default App;
